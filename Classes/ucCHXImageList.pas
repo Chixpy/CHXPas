072cb7169920b44765814a2cd268085d0d08b2e0
Chixpy <chixpy@gmail.com>
2016-11-14 16:58:28 +0100

    ImageList: Better boolean for Map

--- Classes/ucCHXImageList.pas
+++ Classes/ucCHXImageList.pas
@@ -21,24 +21,24 @@ type
 
   cCHXImageMap= class (specialize TFPGMapObject<string, TPicture>)
    public
-    function AddImageFile(aKey, aFile: String): Integer;
+    function AddImageFile(aKey, aFile: String): Boolean;
   end;
 implementation
 
 { cCHXImageMap }
 
-function cCHXImageMap.AddImageFile(aKey, aFile: String): Integer;
+function cCHXImageMap.AddImageFile(aKey, aFile: String): Boolean;
 var
   Img: TPicture;
 begin
-  Result := 1;
+  Result := True;
     Img := TPicture.Create;
   try
     Img.LoadFromFile(aFile);
   except
     // WOOPS, it can't be loaded.
     FreeAndNil(Img);
-    Result := -1;
+    Result := False; // Not added.
   end;
   Self.AddOrSetData(aKey, Img);
 end;
