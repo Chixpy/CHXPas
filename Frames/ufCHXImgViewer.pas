49f693e0e9884c5e7e98dfb0d8a780b38984469f
Chixpy <chixpy@gmail.com>
2016-12-12 16:29:09 +0100

    w Working in Image Viewer frame...

--- Frames/ufCHXImgViewer.pas
+++ Frames/ufCHXImgViewer.pas
@@ -5,9 +5,10 @@ unit ufCHXImgViewer;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Menus, ComCtrls,
-  Spin, StdCtrls,
-  ExtCtrls, ActnList;
+  Classes, SysUtils, LazFileUtils, Forms, Controls, Menus, ComCtrls,
+  Spin, StdCtrls, LazUTF8,
+  ExtCtrls, ActnList,
+  uCHXStrUtils, uCHXImageUtils;
 
 type
 
@@ -46,19 +47,30 @@ type
     mZoomOut: TMenuItem;
     pmAction: TPopupMenu;
     sbInfo: TStatusBar;
+    ScrollBox1: TScrollBox;
     Splitter: TSplitter;
     tbClose: TToolButton;
     tbImage: TToolBar;
     tbOriginalSize: TToolButton;
     tbZoomIn: TToolButton;
     tbZoomOut: TToolButton;
+    procedure actFirstExecute(Sender: TObject);
+    procedure actLastExecute(Sender: TObject);
+    procedure actStretchExecute(Sender: TObject);
 
   private
     FIconsIniFile: TFilename;
+    FImageIndex: integer;
     procedure SetIconsIniFile(AValue: TFilename);
+    procedure SetImageIndex(AValue: integer);
+
+  protected
+    procedure ChangeImage;
+    procedure UpdateStatusBar;
 
   public
     property IconsIniFile: TFilename read FIconsIniFile write SetIconsIniFile;
+    property ImageIndex: integer read FImageIndex write SetImageIndex;
 
     procedure AddImages(aImageList: TStrings; Index: integer = 0);
     procedure AddImage(aImageFile: TFilename; aObject: TObject = nil);
@@ -73,20 +85,78 @@ implementation
 
 { TfmCHXImgViewer }
 
+procedure TfmCHXImgViewer.actFirstExecute(Sender: TObject);
+begin
+  ImageIndex := 1;
+end;
+
+procedure TfmCHXImgViewer.actLastExecute(Sender: TObject);
+begin
+  ImageIndex := lbxFiles.Items.Count;
+end;
+
+procedure TfmCHXImgViewer.actStretchExecute(Sender: TObject);
+begin
+  Image.Stretch := actStretch.Checked;
+end;
+
 procedure TfmCHXImgViewer.SetIconsIniFile(AValue: TFilename);
 begin
   FIconsIniFile := SetAsFile(AValue);
-  ReadActionsIcons(IconsIni, Self.Name, ilActions, ActionList);
+  ReadActionsIcons(FIconsIniFile, Self.Name, ilActions, ActionList);
+end;
+
+procedure TfmCHXImgViewer.SetImageIndex(AValue: integer);
+begin
+  if FImageIndex = AValue then
+    Exit;
+  FImageIndex := AValue;
+  ChangeImage;
+end;
+
+procedure TfmCHXImgViewer.ChangeImage;
+var
+  aFilename: string;
+begin
+  if lbxFiles.Items.Count = 0 then
+  begin
+    Image.Picture.Clear;
+    Exit;
+  end;
+
+  aFilename := lbxFiles.Items[ImageIndex - 1];
+
+  if FileExistsUTF8(aFilename) then
+  begin
+    Image.Picture.LoadFromFile(aFilename);
+    UpdateStatusBar;
+  end
+  else
+  begin
+    Image.Picture.Clear;
+    lbxFiles.Items.Delete(ImageIndex - 1);
+    ChangeImage;
+  end;
+end;
+
+procedure TfmCHXImgViewer.UpdateStatusBar;
+begin
+  sbInfo.Panels[1].Text :=
+    IntToStr(Image.Picture.Width) + 'x' + IntToStr(Image.Picture.Height);
+  if ImageIndex > 0 then
+    sbInfo.Panels[2].Text := lbxFiles.Items[ImageIndex - 1];
 end;
 
 procedure TfmCHXImgViewer.AddImages(aImageList: TStrings; Index: integer);
 begin
+  Index := Index + lbxFiles.Items.Count;
   lbxFiles.Items.AddStrings(aImageList);
+  ImageIndex := Index;
 end;
 
 procedure TfmCHXImgViewer.AddImage(aImageFile: TFilename; aObject: TObject);
 begin
-  lbxFiles.Items.AddObject(aImageFile, aObject);
+  ImageIndex := lbxFiles.Items.AddObject(aImageFile, aObject);
 end;
 
 constructor TfmCHXImgViewer.Create(TheOwner: TComponent);
